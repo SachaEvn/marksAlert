ffde39522bf7343647395a0d7d83890f691d5961
LimeSku <66lime7@protonmail.com>
2022-04-02 16:27:38 +0200

    remove useless files

--- index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const config = require("./config.json");
-
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-
-client.login(config.BOT_TOKEN);
-
-
-const url = 'https://aurion.junia.com'
